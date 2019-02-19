e7fc987c84848482e16eff13a13b670de118ce00
Kose-i <tamura.kosei905@mail.kyutech.jp>
2019-02-19 18:54:51 +0900

    Add button callback

--- src/window.py
+++ src/window.py
@@ -13,21 +13,24 @@ if __name__=='__main__':
   your_window.resize(450, 600)
   your_window.setWindowTitle("your_window")
 
-  enemy_window = QWidget()
-  enemy_window.resize(300, 300)
-  enemy_window.setWindowTitle("enemy_window")
+#  enemy_window = QWidget()
+#  enemy_window.resize(300, 300)
+#  enemy_window.setWindowTitle("enemy_window")
 
-  button_box =[]
-  def change_title(i=0, j=0):
-    button_box[i*9+j].setText('1')
+  button_box = [[QPushButton("", your_window) for i in range(0,9)] for j in range(0,9)]
+
+  def change_button(i,j):
+    button_box[i][j].setText('1')
 
   for i in range(0, 9):
     for j in range(0,9):
-      button_box.append(QPushButton("", your_window))
-      button_box[i*9 +j].setGeometry(50*i, 50*j, 50, 50)
-      button_box[i*9 +j].clicked.connect(change_title(i,j))
+      button_box[i][j].setGeometry(50*i, 50*j, 50, 50)
+
+  for i in range(0, 9):
+    for j in range(0,9):
+      button_box[i][j].clicked.connect(lambda: change_button(i,j))
 
   your_window.show()
-  enemy_window.show()
+#  enemy_window.show()
 
   app.exec_()
