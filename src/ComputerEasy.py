e45abf67725bd21cc05bb8fceccfe6c5f679ed64
Kose-i <tamura.kosei905@mail.kyutech.jp>
2019-02-26 16:19:07 +0900

    Finish ComputerEasy

--- src/ComputerEasy.py
+++ src/ComputerEasy.py
@@ -20,6 +20,7 @@ class ComputerEasy(EnemyWindow.EnemyWindow):
                     for l in range(0,9):
                         if self.cell_num[j][i] != l+1:
                             self.__cell_list[i][j].remove(l+1)
+
     def del_line_cell_list(self, target_num, line_num):
         for i in range(0,9):
             if len(self.__cell_list[line_num][i]) != 1 and self.__cell_list[line_num][i].count(target_num) != 0:
@@ -35,6 +36,7 @@ class ComputerEasy(EnemyWindow.EnemyWindow):
             for j in range(0,3):
                 if len(self.__cell_list[block_line+i][block_row+j]) != 1 and self.__cell_list[block_line+i][block_row+j].count(target_num) != 0:
                     self.__cell_list[block_line+i][block_row+j].remove(target_num)
+
     def update_first_step(self):
         for i in range(0,9):
             for j in range(0,9):
@@ -43,6 +45,44 @@ class ComputerEasy(EnemyWindow.EnemyWindow):
                     self.del_line_cell_list(self.__cell_list[i][j][0], i)
                     self.del_block_cell_list(self.__cell_list[i][j][0], i, j)
 
+    def set_row_cell_list(self, x_pos, y_pos, num):
+        for i in range(0,9):
+            if i != x_pos and self.__cell_list[i][y_pos].count(num) != 0:
+                return
+        tmp = []
+        tmp.append(num)
+        self.__cell_list[x_pos][y_pos] = tmp
+
+    def set_line_cell_list(self, x_pos, y_pos, num):
+        for i in range(0,9):
+            if i != y_pos and self.__cell_list[x_pos][i].count(num) != 0:
+                return
+        tmp = []
+        tmp.append(num)
+        self.__cell_list[x_pos][y_pos] = tmp
+
+    def set_block_cell_list(self, x_pos, y_pos, num):
+        block_line =3*( x_pos // 3)
+        block_row = 3*( y_pos // 3)
+        for i in range(0,3):
+            for j in range(0,3):
+                if block_line+i == x_pos and block_row+j == y_pos:
+                    continue
+                if self.__cell_list[block_line+i][block_row+j].count(num) != 0:
+                    return
+        tmp = []
+        tmp.append(num)
+        self.__cell_list[x_pos][y_pos] = tmp
+
+    def update_second_step(self):
+        for i in range(0,9):
+            for j in range(0,9):
+                for k in range(1,10):
+                    if len(self.__cell_list[i][j]) != 1 and self.__cell_list[i][j].count(k) != 0:
+                        self.set_row_cell_list(i, j, k)
+                        self.set_line_cell_list(i, j, k)
+                        self.set_block_cell_list(i, j, k)
+
     def update_button_print(self):
         self.__now_open = 0
         for i in range(0,9):
@@ -50,7 +90,8 @@ class ComputerEasy(EnemyWindow.EnemyWindow):
                 if len(self.__cell_list[i][j]) == 1:
                     self.set_button_number(i, j, self.__cell_list[i][j][0])
                     self.__now_open += 1
-        print(self.__now_open)
+        #print(self.__now_open)
+
     def print_cell_list(self):
         for i in range(0,9):
             for j in range(0,9):
@@ -61,6 +102,7 @@ class ComputerEasy(EnemyWindow.EnemyWindow):
         for i in range(0,10):
           self.update_first_step()
           self.update_button_print()
+          self.update_second_step()
 
 
 if __name__=='__main__':
