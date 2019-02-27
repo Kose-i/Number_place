5ae41027b7e92406a52b5161b46f2b19a60b536a
Kose-i <tamura.kosei905@mail.kyutech.jp>
2019-02-27 11:30:24 +0900

    Use command line args

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import time
+import sys
 
 import PlayerWindow
 import ProblemChangeList as probtolist
@@ -11,9 +12,15 @@ def main():
 
 if __name__=='__main__':
     main()
+    prob_str = ""
+    if len(sys.argv) == 2:
+      prob_str = sys.argv[1]
+    else:
+      prob_str = "../problem/problem1.txt"
+
     player = PlayerWindow.PlayerWindow()
     enemy = ComputerEasy.ComputerEasy()
-    problem = probtolist.prob_to_list("../problem/problem1.txt")
+    problem = probtolist.prob_to_list(prob_str)
     player.set_problem(problem)
     enemy.set_problem(problem)
     player.run()
