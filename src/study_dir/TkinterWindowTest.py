dc44e1b12995177b46506da30fe4ad48d56926b4
Kose-i <tamura.kosei905@mail.kyutech.jp>
2019-03-04 09:21:06 +0900

    Add tkinter two window test

---
+++ src/study_dir/TkinterWindowTest.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import tkinter as tk
+
+root = tk.Tk()
+
+root.geometry('800x460')
+
+root.title('player')
+
+
+root2 = tk.Tk()
+
+root2.geometry('800x460')
+
+root2.title('enemy')
+
+root.mainloop()
+#root2.mainloop()
