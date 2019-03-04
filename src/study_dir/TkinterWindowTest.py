4b96aee1af84e01da77c05f401d25e0836c8af30
Kose-i <tamura.kosei905@mail.kyutech.jp>
2019-03-04 09:46:46 +0900

    Refactoring study two window bringup

--- src/study_dir/TkinterWindowTest.py
+++ src/study_dir/TkinterWindowTest.py
@@ -2,18 +2,14 @@
 
 import tkinter as tk
 
+
 root = tk.Tk()
-
 root.geometry('800x460')
-
+font = ('Helvetica, 14')
 root.title('player')
 
-
 root2 = tk.Tk()
-
 root2.geometry('800x460')
-
 root2.title('enemy')
 
 root.mainloop()
-#root2.mainloop()
